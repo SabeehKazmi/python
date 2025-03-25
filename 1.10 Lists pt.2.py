bafba349c16c0b193eaef0aaca29d94555f4eb80
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-03-26 00:04:26 +0500

    1/4

--- 1.10 Lists pt.2.py
+++ 1.10 Lists pt.2.py
@@ -1,15 +1,16 @@
-cats = []
-while True:
-    print('Enter your cat\'s name or press \'q\' to quit either enter an existing name to remove it: ')
-    user_input = input()
-    if user_input == 'q':
-        for i in range(len(cats)):
-            print(cats[i], end=' ---> ')
-        break
-    else:
-        for i in range(len(cats)):
-            if user_input == cats[i]:
-                print('Cat already exsits... meaow!')
-                cats.remove(user_input)
-                break
-        cats.append(user_input)
+# cats = []
+# while True:
+#     print('Enter your cat\'s name or press \'q\' to quit either enter an existing name to remove it: ')
+#     user_input = input()
+#     if user_input == 'q':
+#         for i in range(len(cats)):
+#             print(cats[i], end=' ---> ')
+#         break
+#     else:
+#         for i in range(len(cats)):
+#             if user_input == cats[i]:
+#                 print('Cat already exsits... meaow!')
+#                 cats.remove(user_input)
+#                 continue # stops the loop or if right there and restarts it
+#         cats.append(user_input)
+
