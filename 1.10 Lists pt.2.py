fd449e772fed59d5730e99781723fc06e10eaa2e
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-03-25 23:59:33 +0500

    1/4

--- 1.10 Lists pt.2.py
+++ 1.10 Lists pt.2.py
@@ -1,3 +1,15 @@
-list = [['A', 12, 2.4], ['B', 13, 2.5]]
-list.append(['C', 14, 2.6])
-print(list)
+cats = []
+while True:
+    print('Enter your cat\'s name or press \'q\' to quit either enter an existing name to remove it: ')
+    user_input = input()
+    if user_input == 'q':
+        for i in range(len(cats)):
+            print(cats[i], end=' ---> ')
+        break
+    else:
+        for i in range(len(cats)):
+            if user_input == cats[i]:
+                print('Cat already exsits... meaow!')
+                cats.remove(user_input)
+                break
+        cats.append(user_input)
