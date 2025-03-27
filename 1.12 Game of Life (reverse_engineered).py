7176f2175cae8d25b29e97b9bab8719183289b00
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-03-27 22:39:35 +0500

    resume working...

--- 1.12 Game of Life (reverse_engineered).py
+++ 1.12 Game of Life (reverse_engineered).py
@@ -27,16 +27,16 @@ WIDTH = 60
 
 
 
-# Create a list of list for the cells:
-nextCells = []
-for x in range(WIDTH):
-    column = [] # Create a new column.
-    for y in range(HEIGHT):
-        if random.randint(0, 1) == 0:
-            column.append('#') # Add a living cell.
-        else:
-            column.append(' ') # Add a dead cell.
-    nextCells.append(column) # nextCells is a list of column lists.
+# # Create a list of list for the cells:
+# nextCells = []
+# for x in range(WIDTH):
+#     column = [] # Create a new column.
+#     for y in range(HEIGHT):
+#         if random.randint(0, 1) == 0:
+#             column.append('#') # Add a living cell.
+#         else:
+#             column.append(' ') # Add a dead cell.
+#     nextCells.append(column) # nextCells is a list of column lists.
 
 # while True: # Main program loop.
 #     print('\n\n\n\n\n') # Separate each step with newlines.
