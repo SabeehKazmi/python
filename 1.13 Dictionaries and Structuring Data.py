10fd63c10cf39332b28596282feb2b7a5df2213a
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-03-29 19:07:44 +0500

    directories completed

--- 1.13 Dictionaries and Structuring Data.py
+++ 1.13 Dictionaries and Structuring Data.py
@@ -1,3 +1,23 @@
 # dictonary 
 _sample = {'cats':'fezi','dogs':'tom'}
-print(_sample['cats'])
+# print(_sample['cats'])
+
+# a complex dictonary
+degree = {
+    'title':'software engineering',
+    'specialization':'cybersecurity',
+    'programming subjects':['c#', 'Python','SQL'],
+    'cgpa':'3.29'
+}
+#print(degree['programming subjects'])
+
+#nested directories
+nest_dir = {
+    'classes':['programming','database','networking','security'],
+    'security':{
+        'subjects':['database sec','network sec','DevSecOp'],
+        'ranking':'3.29'
+    }
+}
+# print(nest_dir['security']['subjects']) # to print subjects within security
+
