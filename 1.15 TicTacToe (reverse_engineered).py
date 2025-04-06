c6d9bc7a98ce62b2809467d7da4168672069be5a
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-06 21:11:26 +0500

    2

---
+++ 1.15 TicTacToe (reverse_engineered).py
@@ -0,0 +1,8 @@
+# need an introduction? I don't think so, let's go straight to the code, brother
+import pprint
+board = {
+    'TL':'', 'TC':'', 'TR':'',
+    'CL':'', 'CC':'', 'CR':'',
+    'BL':'', 'BC':'', 'BR':''
+}
+pprint.pprint(board)
