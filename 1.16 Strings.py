d2d83a202cec6b47af76f99724aeed0792880a9f
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-07 19:49:43 +0500

    2/4

--- 1.16 Strings.py
+++ 1.16 Strings.py
@@ -1,1 +1,13 @@
 # manipulating strings
+# we are already familiar with \' \n \t \\ etc. but here's a new way 
+print(r'C:\users\remnux\python')
+# instead of \\ everytime, we will just use 'r' for specifying paths and anything contains \
+
+# triple quotes | in triple quotes any shananigans go lile:
+print('''
+      Hi, Anna!
+        As stupid as it sound, your cat is a spy. \ a spy... sp..
+      Regards,
+      u/man_with_a_mullet
+''')
+
