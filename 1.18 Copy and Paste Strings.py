d50319e3368461657ba8b3a10703850b89b493ff
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-13 23:18:49 +0500

    1/4

--- 1.18 Copy and Paste Strings.py
+++ 1.18 Copy and Paste Strings.py
@@ -1,1 +1,14 @@
 # copying and pasting strings with pyperclip() module
+# 1. in terminal, first install pyperclip
+# > pip install pyperclip
+
+# 2. import pyperclip in your python program
+import pyperclip
+
+# copy to clipboard
+pyperclip.copy("Hello, Friend!")
+
+# copy from clipboard
+text = pyperclip.paste()
+print(text)
+
