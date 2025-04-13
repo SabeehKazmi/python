63d17165fd0ff4bca3e766ce1d78170317a1dc67
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-14 00:05:49 +0500

    0/4

---
+++ 1.20 Py Latin (reverse_engineered).py
@@ -0,0 +1,4 @@
+# pig latin is a madup language that alters english words
+print('Enter something to be convered into Py Latin:\n')
+message=input()
+VOWELS=['a','e','i','o','u','y'] # 'y' is sometimes a vowel (learn more from your fingers on a keyboard)
