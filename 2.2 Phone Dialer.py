ffe8ea08c01d3c58f824e26504bd5a19ee3938d4
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-14 06:33:07 +0500

    complete

--- 2.2 Phone Dialer.py
+++ 2.2 Phone Dialer.py
@@ -2,3 +2,28 @@ import msvcrt, winsound, re
 # msvcrt for reading the keystoke without pressing enter
 # winsound for sounds
 # re for checking a format
+
+def play(frequency, duration=200):
+    winsound.Beep(frequency, duration)
+
+def isvalid(number):
+    return re.fullmatch(r'\d{4}-\d{7}',number)
+
+print('Dial a number: (format: 0300-0000000)')
+p_no=''
+
+while True:
+    p_no_digit=msvcrt.getch().decode('utf-8')
+    if p_no_digit.isdigit() or p_no_digit=='-':
+        print(p_no_digit, end='', flush=True)
+        p_no+=p_no_digit
+        play(600)
+    elif p_no_digit=='\r':
+        print()
+        if isvalid(p_no):
+            print("Valid!")
+            play(800)
+        else:
+            print('Invalid!')
+            play(400)
+        break
