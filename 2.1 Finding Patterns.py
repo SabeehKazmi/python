dbd17ff0dc161bc2dabdf6948f0c745cc8ac256c
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-14 06:00:11 +0500

    1/4

---
+++ 2.1 Finding Patterns.py
@@ -0,0 +1,15 @@
+# let's find a phone no in a string
+data={
+    'name':'John',
+    'cell-no':'0300-0000000',
+    'cnic-no':'333-33-3333333-3'
+}
+for a,b in data.items():
+    if len(b) == 12:
+        if b[4] == '-':
+            print(f'Phone no: {b}')
+        else:
+            print('Error 404: Not Found!')
+'''
+For a fact we know that a phone number is 12 in length with a '-' ( a PAK phone_no)
+'''
